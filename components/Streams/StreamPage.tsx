47a94136c09cd89e40e7603bc744f29a7da9fa00
EribyteVT <eribytevt@gmail.com>
2025-04-02 23:22:39 -0500

    fix commit

--- components/Streams/StreamPage.tsx
+++ components/Streams/StreamPage.tsx
@@ -16,7 +16,7 @@ import { PageContainer } from "@/components/ui/page-container";
 import { SectionHeader } from "@/components/ui/selection-header";
 import { Button } from "@/components/ui/button";
 import { LoadingIndicator } from "@/components/ui/loading-indicator";
-import { WebsiteGenerator } from "@/components/WebsiteGenerator/WebsiteGenerator";
+import { WebsiteGenerator } from "@/components/websiteGenerator/WebsiteGenerator";
 
 export default function StreamPage({
   session,
